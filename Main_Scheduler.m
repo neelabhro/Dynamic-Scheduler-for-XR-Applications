d67ad91bb1a43493b02f56ee6a3bf3ef77b23119
Neelabhro Roy <neelabhro16171@iiitd.ac.in>
2022-04-21 11:50:24 +0200

    Frame-Packet mapping done

--- Main_Scheduler.m
+++ Main_Scheduler.m
@@ -6,16 +6,6 @@ close all;
 %Where the data(UDP) payload is 1.278kB
 
 initializations
-n_pack_burst = Burst_Size./1320;       %Number of packets per burst 
-s_no = zeros(length(n_pack_burst),max(round(n_pack_burst)));
-
-for i = 1:length(n_pack_burst)
-    s_no(i, 1:max(round(n_pack_burst(i)))) = linspace(1, round(n_pack_burst(i)), round(n_pack_burst(i)));    
-end    
-A = linspace(1, round(n_pack_burst(2)), round(n_pack_burst(2)));
-
-
-
 
 btime = t_sorted_nxt_frame;
 [tatime, wtime, b, t] = Round_Robin_Scheduler(btime, n, q);
@@ -24,6 +14,6 @@ btime = t_sorted_nxt_frame;
 
 
 
-plot(1:length(Initial_QoE),Initial_QoE);
+%plot(1:length(Initial_QoE),Initial_QoE);
 %figure;
-title('QoE variations across varying bursts')
+%title('QoE variations across varying bursts')
--- initializations.m
+++ initializations.m
@@ -2,19 +2,21 @@ traceFile = readmatrix('ge_cities_40mbps_60fps'); %Google VR trace file
 Burst_Size = traceFile(1 : end, 1);  %Represents the trace burst sizes in Bytes
 t_nxt_frame = traceFile(1 : end, 2); %Represents the time to next arriving frame in seconds
 t_deadline = zeros(length(t_nxt_frame),1);
+
 for i=1:length(t_deadline)
     t_deadline(i+1) = t_nxt_frame(i) + t_deadline(i);
 end    
 t_deadline = t_deadline(1:length(t_nxt_frame));
 
-N = 2;
 n = 100;
 q = 0.016;            %quantum time- a round-robin scheduler generally employs time-sharing, giving each job a time slot or quantum
+
 
 n_pack_burst = Burst_Size./1320;       %Number of packets per burst 
 Initial_QoE = floor(n_pack_burst./(1000.*t_nxt_frame)); %Preliminarily defining this to be a
 %fraction of the total number of packets encompassing a frame and taking
 %the time to next frame into account as well
+
 
 %Sort the Scheduled frames according to the QoEs
 [sorted_QoE, sortQoEIdx] = sort(Initial_QoE,'descend');
@@ -23,3 +25,26 @@ Initial_QoE = floor(n_pack_burst./(1000.*t_nxt_frame)); %Preliminarily defining 
 %A = find(Initial_QoE==15); Testing the correctness
 sorted_burst = Burst_Size(sortQoEIdx);
 t_sorted_nxt_frame = t_nxt_frame(sortQoEIdx);
+
+
+
+
+
+
+
+n_pack_burst = Burst_Size./1320;       %Number of packets per burst 
+s_no = zeros(length(n_pack_burst),max(round(n_pack_burst)));
+
+for i = 1:length(n_pack_burst)
+    s_no(i, 1:max(round(n_pack_burst(i)))) = linspace(1, round(n_pack_burst(i)), round(n_pack_burst(i)));    
+end    
+[row,column] = size(s_no);
+A = linspace(1, round(n_pack_burst(2)), round(n_pack_burst(2)));
+
+s_no = s_no(:).';
+%s_no_numerals = ones(1,length(n_pack_burst));
+s_no_frames = repelem(linspace(1, length(n_pack_burst), length(n_pack_burst)) , column);
+
+k = find(s_no);
+frame_packet_mapper = transpose([s_no_frames(k); s_no(k)]);
+B = frame_packet_mapper(1:10000,:);

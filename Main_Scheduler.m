d48876b6008b98868c37f8ab284e840f1923fc6f
Neelabhro Roy <neelabhro16171@iiitd.ac.in>
2022-04-21 10:52:51 +0200

    Including packets corresponding to each frame

--- Main_Scheduler.m
+++ Main_Scheduler.m
@@ -3,16 +3,27 @@ clc;
 close all;
 
 %Diving into packet level scheduling, each packet is 1.32kB 
-%Where the data(UDP) payload is 1   .278kB
+%Where the data(UDP) payload is 1.278kB
 
 initializations
+n_pack_burst = Burst_Size./1320;       %Number of packets per burst 
+s_no = zeros(length(n_pack_burst),max(round(n_pack_burst)));
+
+for i = 1:length(n_pack_burst)
+    s_no(i, 1:max(round(n_pack_burst(i)))) = linspace(1, round(n_pack_burst(i)), round(n_pack_burst(i)));    
+end    
+A = linspace(1, round(n_pack_burst(2)), round(n_pack_burst(2)));
+
+
+
 
 btime = t_sorted_nxt_frame;
 [tatime, wtime, b, t] = Round_Robin_Scheduler(btime, n, q);
 %[tatime, wtime, t1, t2] = Scheduler_FCFS(btime, n)
 
+
+
+
 plot(1:length(Initial_QoE),Initial_QoE);
-figure;
+%figure;
 title('QoE variations across varying bursts')
-
-

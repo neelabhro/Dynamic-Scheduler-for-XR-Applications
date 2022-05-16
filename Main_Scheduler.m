a40702372c1d2ef56bd0f13ede5f40d53dc4a96c
Neelabhro Roy <neelabhro16171@iiitd.ac.in>
2022-05-16 15:51:16 +0200

    Add files via upload

--- Main_Scheduler.m
+++ Main_Scheduler.m
@@ -6,18 +6,35 @@ close all;
 %Where the data(UDP) payload is 1.278kB
 
 initializations
-Initial_Queue = containers.Map(s_no_frames, t_deadline_packets);
+%Common_Queue = zeros(200,2);
+time_slots = 0:0.034:2;
+Virtual_Queue{1} = zeros(length(time_slots),1);
+Virtual_Queue{2} = zeros(length(time_slots),1);
+
+for i = 1:length(time_slots)
+    for j = 1:num_users
+        if t_arrival(i,j) < time_slots(i)
+            fprintf('Hello')
+            Virtual_Queue{j}(i) = t_arrival(i,j);
+        end    
+    end
+end    
+%Initial_Queue = containers.Map(s_no_frames, t_deadline_packets);
 % While new arrival event happens:
-Initial_Queue(length(s_no_frames) +1) = 5;
+%Initial_Queue(length(s_no_frames) +1) = 5;
 %Update the length of the frame_vector
 %s_no_frames = length(Initial_Queue);
 
 %Remove the elements of the queue if needed
 %remove(Initial_Queue,scheduled_packet)
 %valueN = Initial_Queue(5);
-btime = t_sorted_nxt_frame;
-
-[tatime, wtime, b, t] = Round_Robin_Scheduler(btime, n, q);
+%btime = t_sorted_nxt_frame;
+%for i=1:100
+%    btime(i) = packet.arrival{i,1};
+%end
+btime = t_arrival_packet;
+[scheduled_order] = FCFS(btime, num_users);
+%[tatime, wtime, b, t] = Round_Robin_Scheduler(btime, n, q);
 %[tatime, wtime, t1, t2] = Scheduler_FCFS(btime, n)
 
 

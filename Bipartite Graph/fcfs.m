5d20da3e76edf485a8aa882b9662f072e6d42e57
Neelabhro Roy <neelabhro16171@iiitd.ac.in>
2022-08-27 20:08:17 +0200

    Testing

--- Bipartite Graph/fcfs.m
+++ Bipartite Graph/fcfs.m
@@ -1,21 +1,33 @@
-function [fcfs_val, users, selected_users] = fcfs(users, selected_users, data_point, slot_length)
+function [fcfs_val, number_of_packets_per_frame, frame_release_times] = fcfs(users, selected_users, n, slot_length)
 
 %Initial definition and concatenation of input data points
+current_packet_index = 0;
+dropped_packet_index = 0;
+scheduled_packet_index = 0;
+total_number_packets = 0;
+
+
 %Dimensions of Selected users is number_of_max_simulated_usersXnum_sim_instances
 %Row number corresponds to the number of users being simulated together
-users = users(selected_users);
-% 
-% current_packet_index = 0;
-% dropped_packet_index = 0;
-% scheduled_packet_index = 0;
-% 
-% users.frame_release_times = [selected_users.frame_release_times ; users.frame_release_times];
-% users.nf = [selected_users.nf ; users.nf];
-% users.frameSizeB = [selected_users.frameSizeB ; users.frameSizeB];
-% users.number_of_packets_per_frame = [selected_users.number_of_packets_per_frame ; users.number_of_packets_per_frame];
-% users.value = [selected_users.value ; users.value];
-% users.fps = [selected_users.fps ; users.fps];
-% 
+number_of_packets_per_frame= {};
+frame_release_times = {};
+frameSizeB = {};
+fps = {};
+value = {};
+nf = {};
+
+for user_id = 1:n
+    number_of_packets_per_frame = [(users{selected_users(user_id)}.number_of_packets_per_frame(1:end-1)) number_of_packets_per_frame ];
+    frame_release_times = [(users{selected_users(user_id)}.frame_release_times(1:end-1)) frame_release_times ];
+    frameSizeB = [(users{selected_users(user_id)}.frameSizeB(1:end-1)) frameSizeB ];
+    value = [(users{selected_users(user_id)}.value(1:end-1)) value ];
+    fps = [(users{selected_users(user_id)}.fps(1:end-1)) fps ];
+    nf = [(users{selected_users(user_id)}.nf(1:end-1)) nf ];
+end
+
+%total_number__of_packets = sum(number_of_packets_per_frame); 
+
+
 % %Sorting the frame release time according to FCFS policy
 % [users.sorted_frame_release_times, sorted_frame_release_times_index] = sort(users.frame_release_times);
 % 
@@ -46,4 +58,4 @@ users = users(selected_users);
 
 
 
-fcfs_val = users;
+fcfs_val = total_number_packets;
---
+++ Bipartite Graph/test_scheduler.m
@@ -0,0 +1,80 @@
+clear;
+clc;
+close all;
+%N is the number of users, i.e., trace files
+
+% Get a list of all files in the folder with the desired file name pattern.
+filePattern = fullfile('Atlantis', '*.mat'); % Change to whatever pattern you need.
+theFiles = dir(filePattern);
+for k = 1 : length(theFiles)
+    baseFileName = theFiles(k).name;
+    fullFileName = fullfile(theFiles(k).folder, baseFileName);
+    fprintf(1, 'Now reading %s\n', fullFileName);
+    % Now do whatever you want with this file name,
+    users_original{k} = load(fullFileName);
+end
+
+
+
+%number of users
+N = 4;
+
+L = length(theFiles);
+%number of simulation insances
+S = 10;
+%slot length in ms(depends on the SCS)
+slot_length = 0.125;
+%maximum release time in ms
+max_release_time = 10;
+%packetsize in B
+packet_size = 1500;
+values_range = [10 50];
+
+for l = 1 : L
+    users_original{l}.number_of_packets_per_frame = ceil(users_original{l}.frameSizeB./packet_size); 
+end
+
+for data_point = 1:N    
+    for sim_instance = 1:S        
+        for n = 1 : N
+            l = randi(L);
+            truncuated_length = ceil(users_original{l}.nf/1000);
+            trace_slice_lower_bound = randi([1 users_original{l}.nf-truncuated_length],1,1);
+            trace_slice_upper_bound = trace_slice_lower_bound + truncuated_length;
+            users_truncuated{n}.avgframeSizekB = users_original{l}.avgframeSizekB;
+            users_truncuated{n}.fps = users_original{l}.fps;
+            users_truncuated{n}.frameSizeB = users_original{l}.frameSizeB(trace_slice_lower_bound:trace_slice_upper_bound-1);
+            users_truncuated{n}.frametype = users_original{l}.frametype(trace_slice_lower_bound:trace_slice_upper_bound-1);
+            users_truncuated{n}.info = users_original{l}.info;
+            users_truncuated{n}.nf = truncuated_length;
+            users_truncuated{n}.number_of_packets_per_frame = users_original{l}.number_of_packets_per_frame(trace_slice_lower_bound:trace_slice_upper_bound-1);
+            users_truncuated{n}.value = randi(values_range,1);
+        end
+        users = users_truncuated;
+        %select a set of users uniformly at random for a simulation scanario
+        selected_users = randperm(N,data_point);
+        
+        %select release times of the first frame for each user uniformly at
+        %random for a simulation scanario
+        %first_frame_release_times = randi([1 max_release_time],1,data_point);
+        first_frame_release_times = unifrnd(1, max_release_time,[1 data_point]);
+        
+        for user_id = 1:data_point
+            fps_increment = (1/users{selected_users(user_id)}.fps)*1000;
+            users{selected_users(user_id)}.frame_release_times = first_frame_release_times(user_id) + fps_increment*(0:users{selected_users(user_id)}.nf-1)'; 
+            users{selected_users(user_id)}.user_id = selected_users(user_id);
+            for frame = 1:users{selected_users(user_id)}.nf - 1
+                users{selected_users(user_id)}.frame_deadline(frame,1) = users{selected_users(user_id)}.frame_release_times(frame+1);                
+            end
+        end
+        
+   
+        [fcfs_val, number_of_packets_per_frame, frame_release_times] = fcfs(users,selected_users,data_point,slot_length);
+        
+        results.fcfs{data_point,sim_instance}.val = fcfs_val;
+        %results.all_packets{data_point,sim_instance}.val = all_packets_value;
+        fprintf('Simulations running for: data point %i and simulation instace %i.\n',data_point,sim_instance);
+    end
+end
+
+

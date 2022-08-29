a6912769289d90ddcc6a030658ebc39a24b21f05
Neelabhro Roy <neelabhro16171@iiitd.ac.in>
2022-08-29 02:56:27 +0200

    Add files via upload

--- Bipartite Graph/fcfs.m
+++ Bipartite Graph/fcfs.m
@@ -1,11 +1,9 @@
-function [fcfs_val, number_of_packets_per_frame, frame_release_times, user, value] = fcfs(users, selected_users, n, slot_length)
+function [fcfs_val, number_of_packets_per_frame, frame_release_times, user, value, total_number_of_packets] = fcfs(users, selected_users, n, slot_length)
 
 %Initial definition and concatenation of input data points
 current_packet_index = 0;
 dropped_packet_index = 0;
 scheduled_packet_index = 0;
-total_number_packets = 0;
-
 
 %Dimensions of Selected users is number_of_max_simulated_usersXnum_sim_instances
 %Row number corresponds to the number of users being simulated together
@@ -15,6 +13,7 @@ frameSizeB = {};
 fps = {};
 value = {};
 nf = {};
+frame_deadline = {};
 
 for user_id = 1:n
     number_of_packets_per_frame = [(users{selected_users(user_id)}.number_of_packets_per_frame(1:end-1)) ; number_of_packets_per_frame ];
@@ -23,30 +22,37 @@ for user_id = 1:n
     value = [repmat((users{selected_users(user_id)}.value),(length(users{selected_users(user_id)}.frameSizeB(1:end-1))),1) ; value ];
     fps = [repmat((users{selected_users(user_id)}.fps),(length(users{selected_users(user_id)}.frameSizeB(1:end-1))),1) ; fps ];
     nf = [repmat((users{selected_users(user_id)}.nf),(length(users{selected_users(user_id)}.frameSizeB(1:end-1))),1) ; nf ];
+    frame_deadline = [(users{selected_users(user_id)}.frame_deadline(1:end)) ; frame_deadline ];
 end
 number_of_packets_per_frame = cat(1, number_of_packets_per_frame{:});
 frame_release_times = cat(1, frame_release_times{:});
+frame_deadline = cat(1, frame_deadline{:});
 frameSizeB = cat(1, frameSizeB{:});
 value = cat(1, value{:});
 fps = cat(1, fps{:});
 nf = cat(1, nf{:});
-%total_number_of_packets = sum(user.number_of_packets_per_frame); 
-
 
 %Sorting the frame release time according to FCFS policy
 [frame_release_times, sorted_frame_release_times_index] = sort(frame_release_times);
 
 % Reordering the existing elements according to the changed order
+
 nf = nf(sorted_frame_release_times_index);
 frameSizeB = frameSizeB(sorted_frame_release_times_index);
 number_of_packets_per_frame = number_of_packets_per_frame(sorted_frame_release_times_index);
 value = value(sorted_frame_release_times_index);
 fps = fps(sorted_frame_release_times_index);
+frame_deadline = frame_deadline(sorted_frame_release_times_index);
 
 user = {};
-user = struct('number_of_packets_per_frame', number_of_packets_per_frame, 'frame_release_times', frame_release_times, 'frameSizeB', frameSizeB, 'fps', fps, 'nf', nf, 'value', value);
+user = struct('number_of_packets_per_frame', number_of_packets_per_frame, 'frame_release_times', frame_release_times, 'frameSizeB', frameSizeB, 'fps', fps, 'nf', nf, 'value', value, 'frame_deadline', frame_deadline);
 
-% total_packets = users.nf * users.number_of_packets_per_frame;
+%frame_release_times = frame_release_times*ones((user.nf) .* (user.number_of_packets_per_frame),1);
+total_number_of_packets = sum(user.number_of_packets_per_frame);
+% for i = 1:length(frame_release_times)
+% %      new_frame_release_times(i) = (frame_release_times(i).*ones(number_of_packets_per_frame(i),1))';
+%     frame_release_times(i) = repmat(frame_release_times(i), number_of_packets_per_frame(i)');
+% end    
 % %Changing frame level data to packet level data for packet level scheduling
 % user.value = user.value*ones(user.nf .* user.number_of_packets_per_frame, 1);
 % for user_id = 1:n

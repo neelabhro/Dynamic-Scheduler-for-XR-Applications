9be39a94763759d56e5e011294dd5a0967759a1b
Neelabhro Roy <neelabhro16171@iiitd.ac.in>
2022-09-12 23:35:48 +0200

    Added comments

--- Bipartite Graph/compute_bipartite_graph.m
+++ Bipartite Graph/compute_bipartite_graph.m
@@ -5,6 +5,8 @@ for user_id = 1:n
     bipartite_graph_dimension = bipartite_graph_dimension + sum(users{selected_users(user_id)}.number_of_packets_per_frame(1:end-1));
     latest_frame = max(latest_frame,users{selected_users(user_id)}.frame_release_times(end));
 end
+%The number of rows correspond to the total number of packets
+%The number of columns correspond to the time slots required
 
 bipartite_graph_matrix = zeros(bipartite_graph_dimension, ceil(latest_frame/slot_length));
 
@@ -19,3 +21,6 @@ for user_id = 1:n
     end
 end
 end
+
+% Mi is the packet number
+% Mj*time slot provides the final time slot in which the packet was slotted
